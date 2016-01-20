251f1c894a4c52a1e06a213b8aa9a2ce7a3219d2
blayzekohime <blayzekohime@gmail.com>
2016-01-20 02:00:20 -0500

    Initial Commit 2

---
+++ Source/Dragon/DragonGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DragonGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRAGON_API ADragonGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
